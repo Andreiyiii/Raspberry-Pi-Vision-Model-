fc031e6f21d559a8eeff193ba83e5c0cf9fedd7c
Andreiyiii <andrei230834@gmail.com>
2026-03-01 02:44:01 +0200

    clarity

--- src/camera.py
+++ src/camera.py
@@ -4,14 +4,13 @@ cam=cv2.VideoCapture(0)
 
 while True:
     ret,frame=cam.read()
-
     if not ret:
         print("Error,no return from camera")
         break
 
     cv2.imshow('Testing camera', frame)
 
-    if cv2.waitKey(1) == 27:  
+    if cv2.waitKey(1) == ord("esc"):  
         break
 
 
