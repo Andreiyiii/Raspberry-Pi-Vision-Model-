feef66a7d7020963d57e49340b930692c0a2e27f
Andreiyiii <andrei230834@gmail.com>
2026-03-07 08:05:57 +0200

    changed class number

--- src/yolo_raspPi.py
+++ src/yolo_raspPi.py
@@ -9,7 +9,6 @@ model = YOLO(model_path, task='detect')
 cam = cv2.VideoCapture(0)
 
 #last state of classes to compare and not spam user with information
-ultimul_semafor_vazut = None
 ultima_banca_vazuta = False
 ultimul_gunoi_vazut = False
 ultimul_obstacol_vazut = False
@@ -24,7 +23,6 @@ while True:
     results = model(frame, stream=True)
     
     #current state of classes
-    semafor_in_cadrul_curent = None
     banca_in_cadrul_curent = False
     gunoi_in_cadrul_curent = False
     obstacol_in_cadrul_curent = False
@@ -34,29 +32,16 @@ while True:
         for box in boxes:
             cls = int(box.cls[0])
             
-            if cls == 0: 
-                semafor_in_cadrul_curent = "ROSU"
-            elif cls == 1: 
-                semafor_in_cadrul_curent = "VERDE"
-            elif cls == 2: # bench 
+            if cls == 0: # bench 
                 banca_in_cadrul_curent = True
-            elif cls == 3: # trashcan 
+            elif cls == 1: # trashcan 
                 gunoi_in_cadrul_curent = True
-            elif cls == 4: # scooter 
+            elif cls == 2: # scooter 
                 obstacol_in_cadrul_curent = True
 
         frame = r.plot()  
 
     # notify user logic
-    
-    if semafor_in_cadrul_curent != ultimul_semafor_vazut:
-        if semafor_in_cadrul_curent == "ROSU":
-            print("Semafor Rosu! Opreste-te.")
-        elif semafor_in_cadrul_curent == "VERDE":
-            print("Semafor Verde! Poti trece.")
-
-        ultimul_semafor_vazut = semafor_in_cadrul_curent
-
 
     if banca_in_cadrul_curent != ultima_banca_vazuta:
         if banca_in_cadrul_curent == True:
