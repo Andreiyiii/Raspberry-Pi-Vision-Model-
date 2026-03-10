0872c5d04e61506d4f4efd2ff0b115c6b2451436
Andreiyiii <andrei230834@gmail.com>
2026-03-10 03:00:24 +0200

    fix training error

--- src/train_raspPi_model.py
+++ src/train_raspPi_model.py
@@ -3,5 +3,15 @@ from ultralytics import YOLO
 model = YOLO('models/yolov8n.pt') 
 
 
+results = model.train(
+    data='data/data.yaml', 
+    epochs=100,        
+    imgsz=512,         
+    degrees=15.0,      
+    mosaic=1.0,        
+    mixup=0.2,         
+    hsv_s=0.5,         
+    fliplr=0.5         
+)
 best_model = YOLO('runs/detect/train2/weights/best.pt')
 best_model.export(format='ncnn', imgsz=512)
